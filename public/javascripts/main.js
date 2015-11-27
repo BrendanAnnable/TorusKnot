fd2cc536eaf5bf7a00674c986bd6ecd9baf460f8
Brendan Annable <Brendan Annable>
2015-11-28 02:14:58 +1100

    Add independent angular velocities to each particle
    Add simjs dependency for normal distribution random number generation.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,113 +1,92 @@
-var canvas = document.getElementById('canvas');
+(function () {
+	'use strict';
 
-var renderer = new THREE.WebGLRenderer({
-	canvas: canvas,
-	antialias: true
-});
+	let canvas = document.getElementById('canvas');
 
-var scene = new THREE.Scene();
-//var camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-var camera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0.1, 1000);
+	var random = new Random();
 
-//var geometry = new THREE.BoxGeometry(1, 1, 1);
-//var material = new THREE.MeshBasicMaterial({
-//	color: '#f00'
-//});
-//var box = new THREE.Mesh(geometry, material);
-//scene.add(box);
+	let renderer = new THREE.WebGLRenderer({
+		canvas: canvas,
+		antialias: true
+	});
 
-//var geometry = new THREE.PlaneBufferGeometry(1, 1);
-//geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0.5, 0.5, 0.5));
-var geometry = new THREE.TextGeometry('1260', {
-	size: 0.05,
-	height: 0.000
-});
-geometry.center();
-var material = new THREE.RawShaderMaterial({
-	vertexShader: [
-		'precision highp float;',
-		'uniform mat4 projectionMatrix;',
-		'uniform mat4 modelViewMatrix;',
-		'attribute vec3 position;',
-		'attribute vec2 uv;',
-		'varying vec2 vUv;',
-		'void main() {',
+	let scene = new THREE.Scene();
+	//let camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+	let camera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0.1, 1000);
+
+	let material = new THREE.RawShaderMaterial({
+		vertexShader: [
+			'precision highp float;',
+			'uniform mat4 projectionMatrix;',
+			'uniform mat4 modelViewMatrix;',
+			'attribute vec3 position;',
+			'void main() {',
+			'gl_PointSize = 1.0;',
 			'gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);',
-			'vUv = uv;',
-		'}'
-	].join('\n'),
-	fragmentShader: [
-		'precision highp float;',
-		'varying vec2 vUv;',
-		'void main() {',
-			//'gl_FragColor = vec4(vUv.x, vUv.y, 0.0, 1.0);',
+			'}'
+		].join('\n'),
+		fragmentShader: [
+			'precision highp float;',
+			'void main() {',
 			'gl_FragColor = vec4(0.2, 0.3, 0.4, 1.0);',
-		'}'
-	].join('\n')
-});
-var plane = new THREE.Mesh(geometry, material);
-//plane.position.set(0.5, 0.5, 0);
-scene.add(plane);
+			'}'
+		].join('\n')
+	});
 
-var pointCloudGeometry = new THREE.Geometry();
-var n = 200000;
-for (var i = 0; i < n; i++) {
-	var vector = new THREE.Vector3(Math.random() - 0.5, Math.random() - 0.5, 0);
-	pointCloudGeometry.vertices.push(vector);
-}
-var pointCloudMaterial = new THREE.PointsMaterial({
-	color: '#334D66', //'#700',
-	size: 1,
-	sizeAttenuation: false
-});
-var pointCloud = new THREE.Points(pointCloudGeometry, pointCloudMaterial);
-scene.add(pointCloud);
+	let n = 200000;
 
-var clock = new THREE.Clock();
+	let geometry = new THREE.Geometry();
+	for (let i = 0; i < n; i++) {
+		let vector = new THREE.Vector3(Math.random() - 0.5, Math.random() - 0.5, 0);
+		geometry.vertices.push(vector);
+	}
 
-camera.position.z = 1;
+	let angularVelocities = [];
+	for (let i = 0; i < n; i++) {
+		let vector = new THREE.Vector3(0, 0, Math.max(0.1, random.normal(0.5, 0.341)));
+		angularVelocities.push(vector);
+	}
 
-requestAnimationFrame(render);
+	let particles = new THREE.Points(geometry, material);
+	scene.add(particles);
 
-function clamp (value) {
-	return Math.min(0.5, Math.max(-0.5, value));
-}
+	let clock = new THREE.Clock();
 
-function render() {
+	camera.position.z = 1;
+
 	requestAnimationFrame(render);
-	var t = clock.getDelta();
 
-	var vertices = pointCloudGeometry.vertices;
-	var period = 2;
-	for (var i = 0, len = vertices.length; i < len; i++) {
-		var a = 2 * Math.PI * Math.random();
-		var ca = Math.cos(a);
-		var sa = Math.sin(a);
-
-		ca = vertices[i].y;
-		sa = -vertices[i].x;
-		//
-		//vertices[i].x = (((vertices[i].x + (t * ca / period)) + 0.5) % 1) - 0.5;
-		//vertices[i].y = (((vertices[i].y + (t * sa / period)) + 0.5) % 1) - 0.5;
-
-		vertices[i].x = clamp(vertices[i].x + t * ca / period);
-		vertices[i].y = clamp(vertices[i].y + t * sa / period);
-	}
-	pointCloudGeometry.verticesNeedUpdate = true;
-
-	//plane.rotation.x += Math.PI * 2 * t / 3;
-	plane.rotation.y += Math.PI * 2 * t / 13;
-	//plane.rotation.z += Math.PI * 2 * t / 7;
-
-	//box.rotation.x += Math.PI * 2 * t / 3;
-	//box.rotation.y += Math.PI * 2 * t / 5;
-	//box.rotation.z += Math.PI * 2 * t / 7;
-
-	if (canvas.width !== canvas.clientWidth || canvas.height !== canvas.clientHeight) {
-		camera.aspect = canvas.clientWidth / canvas.clientHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+	function clamp(value) {
+		return Math.min(0.5, Math.max(-0.5, value));
 	}
 
-	renderer.render(scene, camera);
-}
+	function render() {
+		requestAnimationFrame(render);
+		let t = clock.getDelta();
+
+		let vertices = geometry.vertices;
+		for (let i = 0, len = vertices.length; i < len; i++) {
+			let a = 2 * Math.PI * Math.random();
+			let ca = Math.cos(a);
+			let sa = Math.sin(a);
+
+			ca = vertices[i].y;
+			sa = -vertices[i].x;
+			//
+			//vertices[i].x = (((vertices[i].x + (t * ca / period)) + 0.5) % 1) - 0.5;
+			//vertices[i].y = (((vertices[i].y + (t * sa / period)) + 0.5) % 1) - 0.5;
+
+			vertices[i].x = clamp(vertices[i].x + t * ca * angularVelocities[i].z);
+			vertices[i].y = clamp(vertices[i].y + t * sa * angularVelocities[i].z);
+		}
+		geometry.verticesNeedUpdate = true;
+
+		if (canvas.width !== canvas.clientWidth || canvas.height !== canvas.clientHeight) {
+			camera.aspect = canvas.clientWidth / canvas.clientHeight;
+			camera.updateProjectionMatrix();
+			renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+		}
+
+		renderer.render(scene, camera);
+	}
+}());

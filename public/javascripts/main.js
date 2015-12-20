fca1aeef34118575b3fea248646ede3b1dabc948
Brendan Annable <brendan@annable.id.au>
2015-12-21 04:17:19 +1100

    Add FPS counter

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -109,10 +109,19 @@
 
 	let controls = new THREE.OrbitControls(camera);
 
+	let stats = new Stats();
+	stats.setMode(0);
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.left = '0';
+	stats.domElement.style.top = '0';
+	document.body.appendChild(stats.domElement);
+
 	requestAnimationFrame(render);
 
 	function render() {
 		requestAnimationFrame(render);
+
+		stats.begin();
 		let t = clock.getDelta();
 
 		material.uniforms.time.value = clock.getElapsedTime();
@@ -135,5 +144,6 @@
 		camera.lookAt(scene.position);
 		controls.update();
 		renderer.render(scene, camera);
+		stats.end();
 	}
 }(jQuery, THREE));

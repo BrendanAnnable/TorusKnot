e38558efdd540c54008d6e78052a6ccab333dc45
Brendan Annable <brendan@annable.id.au>
2015-12-28 22:44:49 +1100

    Remove dead code and change FOV to 75

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -28,18 +28,16 @@
 
 	var gui = new dat.GUI();
 
-	let random = new Random();
-
 	let renderer = new THREE.WebGLRenderer({
 		canvas: canvas,
-		antialias: true,
-		alpha: true
+		antialias: true
 	});
 	renderer.setClearColor('#000');
 
 	let scene = new THREE.Scene();
-	let camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+	let camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
 	//let camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000);
+	camera.position.z = 5.5;
 
 	var settings = {
 		'pointSize': 10,
@@ -57,7 +55,8 @@
 		'numTwists': 64,
 		'numCoils': 3,
 		'numLoops': 8,
-		'spinningSpeed': 1 / 4
+		'spinningSpeed': 1 / 4,
+		'normals': false
 	};
 	let material = new THREE.RawShaderMaterial({
 		uniforms: {
@@ -77,18 +76,12 @@
 			numTwists: {type: 'f', value: settings.numTwists},
 			numCoils: {type: 'f', value: settings.numCoils},
 			numLoops: {type: 'f', value: settings.numLoops},
-			spinningSpeed: {type: 'f', value: settings.spinningSpeed}
+			spinningSpeed: {type: 'f', value: settings.spinningSpeed},
+			normals: {type: 'i', value: settings.normals}
 		},
 		vertexShader: get('shaders/shader.vert'),
 		fragmentShader: get('shaders/shader.frag')
 	});
-	material.transparent = true;
-	//material.depthTest = false;
-	//material.depthWrite = false;
-	//material.blending = THREE.CustomBlending;
-	//material.blendSrc = THREE.SrcAlphaFactor;
-	//material.blendDst = THREE.OneMinusSrcAlphaFactor;
-	//material.blendEquation = THREE.AddEquation;
 	gui.add(settings, 'spinningSpeed', 0, 1).onChange(function (value) {
 		material.uniforms.spinningSpeed.value = value;
 	});
@@ -97,6 +90,9 @@
 	//});
 	gui.add(settings, 'lighting').onChange(function (value) {
 		material.uniforms.uLighting.value = value;
+	});
+	gui.add(settings, 'normals').onChange(function (value) {
+		material.uniforms.normals.value = value;
 	});
 	gui.add(settings, 'numBumps', 0, 10).step(1).onChange(function (value) {
 		material.uniforms.numBumps.value = value;
@@ -138,76 +134,13 @@
 		material.uniforms.thirdColor.value = new THREE.Color(value).toArray();
 	});
 
-	//let n = 100000;
-	//let n = 1000000;
-	//
-	//let geometry = new THREE.Geometry();
-	//
-	//for (let i = 0; i < n; i++) {
-	//	let theta = random.uniform(0, Math.TAU);
-	//	let phi = random.uniform(0, Math.TAU);
-	//	//let delta = random.uniform(0, Math.TAU);
-	//	let delta = random.uniform(0, 1);
-	//	//let delta = random.normal(1.0, 0.3);
-	//	//let mean = 0.05;//0.05 * Math.sin(7 * (phi - 4 * theta)) + 0.3;
-	//	//let mean = 0.01 * Math.sin(7 * (phi - 16 * theta)) + 0.20;
-	//	//let mean = 0.01 * Math.sin(7 * (phi - 16 * theta)) + 0.20;
-	//	//let mean = 0.2;
-	//	//let distance = 0.2;// random.normal(mean, 0.05);
-	//	let vector = new THREE.Vector3(theta, phi, delta);
-	//
-	//	geometry.vertices.push(vector);
-	//}
-
 	let geometry = new THREE.PlaneBufferGeometry(Math.TAU, Math.TAU, 100, 15000);
-
-	let particles = new THREE.Mesh(geometry, material);
-	//let particles = new THREE.Line(geometry, material);
-	//let particles = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-	//	side: THREE.DoubleSide
-	//}));
-	scene.add(particles);
-
-	let n2 = 500000;
-
-	let geometry2 = new THREE.Geometry();
-
-	for (let i = 0; i < n2; i++) {
-		let theta = random.uniform(0, 2 * Math.PI);
-		let phi = random.uniform(0, Math.TAU);
-		//let mean = 0.05;//0.05 * Math.sin(7 * (phi - 4 * theta)) + 0.3;
-		let distance = 0.0; //random.normal(0.3, 0.00);
-		let vector = new THREE.Vector3(theta, distance * Math.cos(phi), distance * Math.sin(phi));
-
-		geometry2.vertices.push(vector);
-	}
-
-	let material2 = new THREE.RawShaderMaterial({
-		uniforms: {
-			time: {type: 'f'}
-		},
-		vertexShader: get('shaders/shader2.vert'),
-		fragmentShader: get('shaders/shader2.frag')
-	});
-	material2.transparent = true;
-	//material.depthTest = false;
-	//material.depthWrite = false;
-	material2.blending = THREE.CustomBlending;
-	material2.blendSrc = THREE.SrcAlphaFactor;
-	material2.blendDst = THREE.OneFactor;
-	material2.blendEquation = THREE.AddEquation;
-
-	let particles2 = new THREE.Points(geometry2, material2);
-	//scene.add(particles2);
+	let mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
 
 	let clock = new THREE.Clock();
 
-	camera.position.z = 8.5;
-
-	let transform = new THREE.Matrix4();
-
 	let controls = new THREE.OrbitControls(camera, canvas);
-
 
 	requestAnimationFrame(render);
 
@@ -215,10 +148,8 @@
 		requestAnimationFrame(render);
 
 		stats.begin();
-		let t = clock.getDelta();
 
 		material.uniforms.time.value = clock.getElapsedTime();
-		material2.uniforms.time.value = clock.getElapsedTime();
 
 		if (canvas.width !== canvas.clientWidth || canvas.height !== canvas.clientHeight) {
 			camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -226,15 +157,6 @@
 			renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
 		}
 
-		//particles.rotation.z += Math.TAU * t / 20;
-		//particles2.rotation.z += Math.TAU * t / 20;
-		//transform.makeRotationX(Math.TAU * t / 30);
-		//camera.position.applyMatrix4(transform);
-		//transform.makeRotationY(Math.TAU * t / 30);
-		//camera.position.applyMatrix4(transform);
-		//transform.makeRotationZ(Math.TAU * t / 70);
-		//camera.position.applyMatrix4(transform);
-		camera.lookAt(scene.position);
 		controls.update();
 		renderer.render(scene, camera);
 		stats.end();
